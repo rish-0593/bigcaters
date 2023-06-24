287a7239c3f607c079cd66606389e1b13609c1b7
Rishabh Kumar <kumarrishabh050@gmail.com>
2023-06-24 15:54:17 +0530

    fix: directory structure

--- php/quote.php
+++ php/quote.php
@@ -6,8 +6,8 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
-require './env.php';
+require '../vendor/autoload.php';
+require '../env.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
